a1af99f243053d7fbbbdf784e798e9136fa917c7
Piotrekk85 <piotr.jastr@gmail.com>
2016-11-30 14:11:54 +0100

    pliki z rzutnika

---
+++ ConsoleApplication21/ConsoleApplication21/NaszWyjatekException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication21
+{
+    class NaszWyjatekException :System.Exception
+    {
+
+        public NaszWyjatekException() { }
+
+        public NaszWyjatekException(string message) 
+            : base(message)
+        {
+
+        }
+
+        public NaszWyjatekException(string message, Exception innerException) 
+          : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApplication21/ConsoleApplication21/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] table = new int[2];
+
+            int liczba1;
+            int liczba2 = 30;
+            int liczba3 = 0;
+            
+            try
+            {
+                //liczba1=liczba2/liczba3;
+                //int[25]=2;
+                //int[13]=liczba2/liczba3;
+                throw new NaszWyjatekException("nasz własny wyjatek");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch
+            {
+                Console.WriteLine("inny wyjątek");
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
